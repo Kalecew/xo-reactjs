f4e43e12e98f104dca8e2081d8f2b62f55b6059f
Kalecew <adamlovelacee@gmail.com>
2022-02-02 18:39:51 +0300

    fix bug

--- src/game.js
+++ src/game.js
@@ -1,16 +1,38 @@
 import {Component} from 'react'
 import Board from './board'
 import Move from './move'
-import calculateWinner from './helpers/calculateWinner.js'
 
 export default class Game extends Component {
   state = {
+    gameOver: false,
     isNextX: true,
     step: 0,
     history: [
       {squares: Array(9).fill(null)}
     ]
   }
+
+  calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,5,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (var i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a]
+      }     
+    }
+    return null
+  }
+
+  createStatus = (winner, isNextX) => winner ? 'Winner: ' + winner : 'Next player: ' + (isNextX ? 'X' : 'O')
 
   createNewHistory = (i) => {
     const {isNextX,step,history} = this.state
@@ -21,14 +43,16 @@ export default class Game extends Component {
     return result
   }
 
-  stopClick = (squares, i) => calculateWinner(squares) || squares[i]
-
-  createStatus = (winner, isNextX) => winner ? 'Winner: ' + winner : 'Next player: ' + (isNextX ? 'X' : 'O')
+  gameIsOver = (squares, i) => {
+    if(this.calculateWinner(squares)) this.setState({gameOver: true})
+    if(this.state.gameOver || this.calculateWinner(squares) || squares[i]) return true
+    return false
+  }
 
   click = (i) => {
-    const {isNextX,step,history} = this.state
+    const {isNextX,step,history,gameOver} = this.state
     const squares = history[step].squares
-    if(this.stopClick(squares, i)) return
+    if(this.gameIsOver(squares, i)) return false   
     const newHistory = this.createNewHistory(i)
     this.setState({
       isNextX: !isNextX,
@@ -37,21 +61,24 @@ export default class Game extends Component {
     })
   }
 
-  renderMoves = () => this.state.history.map((item, i) => <Move i={i} jumpTo={() => this.jumpTo(i)}/>)
+  renderMoves = () => 
+    this.state.history.map((item, i) => 
+      <Move key={i} i={i} jumpTo={() => this.jumpTo(i)}/>
+    )
+
+  calculateIsNextX = (step) => (step % 2) ? false : true
 
   jumpTo = (i) => {
-    const {step, history} = this.state
     this.setState({
-      isNextX: (step % 2) ? false : true,
-      step: i,
-      history: history
+      isNextX: this.calculateIsNextX(i),
+      step: i
     })
   }
 
   render() {
     const {isNextX,step,history} = this.state
     const squares = history[step].squares
-    const winner = calculateWinner(squares)
+    const winner = this.calculateWinner(squares)
     const status = this.createStatus(winner,isNextX)
     
     return (
